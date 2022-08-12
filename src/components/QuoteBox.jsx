570b99cba5efb848635cba96e90ea9274294e14e
Luis David <luischinchilla12364@gmail.com>
2022-08-11 22:44:24 -0400

    entregable1.2

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,33 +1,37 @@
-// --------- Import React
-import React from 'react'
-
-// --------- Component Definition
+import React from "react";
 
 // --------- destructuring Props by elements
-const QuoteBox = ({randomQuote, randomColors, getRandomAll}) => {
+const QuoteBox = ({ randomQuote, randomColors, getRandomAll }) => {
   // --------- Props asign to function
   const objectStyle = {
-    color: randomColors
-  }
+    color: randomColors,
+  };
 
   const objectBgStyle = {
-    backgroundColor: randomColors
-  }
-// --------- JSX Return
-// --------- Paint a card and use props to renderize the view whit the json object
+    backgroundColor: randomColors,
+  };
+  // --------- JSX Return
+  // --------- Paint a card and use props to renderize the view whit the json object
   return (
-    <article className='card' style={objectStyle}>
-      <div className='card__container'>
+    <article className="card" style={objectStyle}>
+      <div className="card__container">
         <i className="fa-solid fa-quote-left quote-icon"></i>
-        <p id='h2' className='card__quote'>{randomQuote.quote}</p>
+        <p id="h2" className="card__quote">
+          {randomQuote.quote}
+        </p>
       </div>
-      <p className='card__author'>{randomQuote.author}</p>
-      <div className='card__btn'>
-        <button onClick={getRandomAll} className='card__btn__arrow' style={objectBgStyle}>&#62;</button>        
+      <p className="card__author">{randomQuote.author}</p>
+      <div className="card__btn">
+        <button
+          onClick={getRandomAll}
+          className="card__btn__arrow"
+          style={objectBgStyle}
+        >
+          &#62;
+        </button>
       </div>
-
     </article>
-  )
-}
+  );
+};
 // --------- Export the component to use on elements or other components
-export default QuoteBox
+export default QuoteBox;
